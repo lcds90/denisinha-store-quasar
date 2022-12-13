9422f3fc1df3bf46cc0c2054c215176358c3e25b
lcds-90 <leonardo.santos@zenvia.com>
2022-12-13 12:47:34 -0300

    chore: installing cypress 🧪

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,54 +1,44 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref } from "vue";
 
-const tab = ref('mails');
-
+const tab = ref("mails");
 </script>
 
 <template>
   <q-layout view="hHh lpR fFf">
-    <q-header
-      bordered
-      class="bg-primary text-white"
-      height-hint="98"
-    >
-      <q-toolbar
-        class="toolbar glossy bg-accent rounded-borders"
-        inset
-      >
+    <q-header bordered class="bg-primary text-white" height-hint="98">
+      <q-toolbar class="toolbar bg-primary rounded-borders" inset>
         <q-toolbar-title>
           <q-avatar square>
-            <img src="../assets/images/logo.jpg">
+            <img
+              src="https://thumbs.gfycat.com/ConventionalOblongFairybluebird-size_restricted.gif"
+            />
           </q-avatar>
-          <span class="text-primary text-weight-bold q-ml-md">Denisinha Store</span>
+          <span class="text-accent text-weight-bold q-ml-md">Lojinha</span>
         </q-toolbar-title>
-        <q-tabs
-          v-model="tab"
-          shrink
-          inline-label
-        >
+        <q-tabs v-model="tab" shrink inline-label>
           <q-route-tab
-            class="bg-secondary q-ma-xs"
+            id="button_home"
+            class="bg-accent q-ma-xs"
             to="/"
             icon="home"
             label="Home"
           />
           <q-route-tab
-            class="bg-secondary q-ma-xs"
+            class="bg-accent q-ma-xs"
             to="/products"
             icon="shopping_cart"
-            label="Loja"
+            label="Carrinho"
           />
           <q-route-tab
-            class="bg-secondary q-ma-xs"
+            class="bg-accent q-ma-xs"
             to="/contact"
             icon="email"
             label="Contato"
           />
         </q-tabs>
       </q-toolbar>
-      <q-tabs>
-      </q-tabs>
+      <q-tabs> </q-tabs>
     </q-header>
     <q-page-container>
       <router-view />
